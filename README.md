25f7a2a288b6d17e02b72211a9957098e953bf40
Marcus Parmentier and Victor Puentes <marcus@>
2017-09-28 20:57:36 -0700

    Add computer funtionality with bugs explained in readme.

--- README.md
+++ README.md
@@ -9,4 +9,8 @@ Specifications
 |4. User can choose to hold to save rolling total    | user hits hold      |  user's total is displayed as 6 to be added on to whatever the total from the next turn. |
 |5. If user rolls a 1, it ends their turn and erases their turn total.| users rolls a one on second turn. | total displayed is still 1.|
 |6.  Once user's total adds to 100, they are declared the winner.   | User's total hits 100      | YOU ARE THE WINNER. |
-|7. User can add a player at beginning of the game | User clicks "add player" button | 
+|7. User can add a player at beginning of the game | User clicks "add player" button |
+
+
+
+| Bugs | 1. Doesn't reflect computer's point total right away, resulting in some loss of points. | 2. If player rolls a 1, computer doesnt react correctly. | 3. Lastly, didnt add the 100 point cap/winner statement. |
--- js/scripts2.js
+++ js/scripts2.js
@@ -1,33 +1,155 @@
 //Business logic*********************************************************
-var pointTotal = 0;
-var roundTotal = 0;
 
-var roll = function () {
+function Player (name, pointTotal) {
+  this.name = name;
+  this.pointTotal = 0;
+}
+
+Player.prototype.getPointTotal = function() {
+  return this.pointTotal;
+}
+
+function Turn () {
+  this.playerOne = new Player ("playerOne");
+  this.computer = new Player ("computer");
+  this.currentPlayer = this.playerOne;
+  this.roundTotal = 0;
+}
+
+Turn.prototype.changePlayer = function () {
+  if (this.currentPlayer.name === "playerOne") {
+    return this.currentPlayer = this.computer;
+  }
+  else {
+    // $("gameTotalCPU").text(currentTurn.computer.pointTotal);
+    return this.currentPlayer = this.playerOne;
+  }
+}
+
+
+Turn.prototype.roll = function () {
   var randomRoll = Math.floor(Math.random() * 6) + 1;
 
   if (randomRoll === 1) {
-    roundTotal = 0;
+    this.roundTotal = 0;
+    this.changePlayer();
+    // if (this.currentPlayer.name === "computer") {
+    //   currentTurn.compute();
+    // };
   }
   else {
-  roundTotal += randomRoll;
-  return randomRoll;
+    this.roundTotal += randomRoll;
   }
+  return randomRoll;
 };
 
-var show = function () {
-
-  return roundTotal;
+Turn.prototype.compute = function () {
+  this.roll();
+  $("#round-totalCPU").text(currentTurn.roundTotal);
+    if (this.roundTotal >= 9) {
+      this.hold();
+    }
+    else if (this.randomRoll === 1) {
+      this.changePlayer();
+    }
+    else {
+      this.compute();
+    }
 };
 
+Turn.prototype.hold = function() {
+  this.currentPlayer.pointTotal += this.roundTotal;
+  $("gameTotalCPU").text(currentTurn.computer.pointTotal);
+  this.changePlayer();
+  this.roundTotal = 0;
+}
 
 
-
-
+var currentTurn = new Turn();
 //User interface logic*****************************************************
-
 $(document).ready(function() {
+
   $("#roll").click(function(event) {
-    $("#currentRollDisplay").text(roll());
-    $("#round-total1").text(show());
+    debugger;
+    $("#currentRollDisplay").text(currentTurn.roll());
+    $("#round-total1").text(currentTurn.roundTotal);
+    if (currentTurn.currentPlayer.name === "computer") {
+      currentTurn.compute();
+    }
+
   });
+
+  $("#hold").click(function(event) {
+    currentTurn.hold();
+    $("#gameTotal1").text(currentTurn.playerOne.pointTotal);
+    $("#gameTotalCPU").text(currentTurn.computer.pointTotal);
+    if (currentTurn.currentPlayer.name === "computer") {
+      currentTurn.compute();
+    };
+  });
+
+
+    document.getElementById("home").onclick = function () {
+      location.href = "index.html";
+    };
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Business logic*********************************************************
+// var pointTotal = 0;
+// var roundTotal = 0;
+//
+// var roll = function () {
+//   var randomRoll = Math.floor(Math.random() * 6) + 1;
+//
+//   if (randomRoll === 1) {
+//     roundTotal = 0;
+//   }
+//   else {
+//   roundTotal += randomRoll;
+//   return randomRoll;
+//   }
+// };
+//
+// var show = function () {
+//
+//   return roundTotal;
+// };
+//
+// var hold = function () {
+//   pointTotal += roundTotal;
+//   return pointTotal;
+// }
+//
+//
+//
+//
+// //User interface logic*****************************************************
+//
+// $(document).ready(function() {
+//   $("#roll").click(function(event) {
+//     $("#currentRollDisplay").text(roll());
+//     $("#round-total1").text(show());
+//   });
+//
+//   $("hold").click(function(event) {
+//     .hold();
+//     $("#gameTotal1")
+//   })
+// });
